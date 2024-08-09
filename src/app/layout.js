454d62dda26be80f959bcbde123f429992fecf9c
Shamil Aliyev <samil.alieff@gmail.com>
2024-08-09 21:26:16 +0200

    changing layout structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Navigation />
       <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   );
 }
