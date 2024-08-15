519440556ca5c00812ea5830503273d4277fbc5d
Shamil Aliyev <samil.alieff@gmail.com>
2024-08-15 14:34:32 +0200

    updated layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navigation from "./components/Navigation";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navigation />
+        <Navbar />
         {children}
         <Footer />
       </body>
