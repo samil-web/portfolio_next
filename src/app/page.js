62c9f597a34c7e663d6db2dc4e9c97b1930a783c
Shamil Aliyev <samil.alieff@gmail.com>
2024-08-15 14:35:25 +0200

    update the main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Book from "@/app/components/Book";
-import Header from "@/app/components/Header";
+import Hero from "@/app/components/Hero";
 import Portfolio from "@/app/components/Portfolio";
 import Testimonial from "@/app/components/Testimonial";
 import Image from "next/image";
 import Questions from "@/app/components/FAQ";
+import Head from "next/head";
+import RecentWork from "./components/RecentWork";
 // import Header, Recent work, testimonial, book a call, FAQ
 // in the header add Logo, Name- left corner, CTA - right corner
 // main proposal mid left side, a visual mid right side and mid down corner
@@ -12,7 +14,12 @@ import Questions from "@/app/components/FAQ";
 export default function Home() {
   return (
     <div>
-      <Header />
+      <Head>
+        <title>BasedMVP</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Hero />
+      <RecentWork />
       <Portfolio />
       <Testimonial />
       <Questions />
