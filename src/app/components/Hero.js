98f650574c07b89c9a0397e6d94a8dc8ca3ebe34
Shamil Aliyev <samil.alieff@gmail.com>
2024-08-15 14:40:02 +0200

    created hero file

---
+++ src/app/components/Hero.js
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <section className="py-20 px-6">
+      <h1 className="text-5xl font-bold mb-4">
+        Get ready to conquer the market with your niche MVP!
+      </h1>
+      <p className="mb-6">
+        We will craft MVP for your target audience who you already know well. We
+        offer you partnership. Your niche knowledge + our technical expertise =
+        success
+      </p>
+      <button className="bg-blue-600 text-white px-6 py-3 rounded">
+        Lets see if you are a fit
+      </button>
+    </section>
+  );
+}
